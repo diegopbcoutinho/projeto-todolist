388382d74e2967ca5ee022b11a24afabd27c454b
Diego Coutinho <105460652+diegopbcoutinho@users.noreply.github.com>
2022-06-19 12:24:14 -0300

    Função para remover itens

--- script/script.js
+++ script/script.js
@@ -5,8 +5,14 @@ function inserir() {
     if (input.value.length == 0) {
         window.alert('Insira um item!')
     } else {
-        res.innerHTML += `- ${input.value} <br>`
+        let item = document.createElement('p')
+        item.innerHTML += `- ${input.value}`
+        res.appendChild(item)
+        item.onclick = function remover() {
+            res.removeChild(item)
+        }
 }
     input.value = ''
     input.focus()
 }
+
